c3e78d64130738faa6edca09a02d5f2db922fa5b
ChantalMorales <chantal.morales@epn.edu.ec>
2020-09-03 14:24:03 -0500

    Añadido a la paágina principal la página de inicio

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ProjectsList from '../components/ProjectsList';
 import { useProjectsList } from '../data/useProjectsList';
 import ShowError from '../components/ShowError';
+import HomePage from '../components/Home';
 
 const IndexPage = () => {
 
   return (
     <>
-
-      <h2>Lista de Proyectos</h2>
-      <ProjectsList />
+      <HomePage/>
+      {/*<h2>Lista de Proyectos</h2>*/}
+      {/*<ProjectsList />*/}
     </>
   );
 };
