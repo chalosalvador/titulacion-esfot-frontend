d3f5348805a7cedff64fbc9bb9c486d929c818e6
Kevin Segovia <kjsegovia144@hotmail.es>
2020-09-25 14:57:47 -0500

    Coreccion ruta

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -8,7 +8,7 @@ const publicRoutes = {
   USERS_ID: `/usuario/:id`,
   INDEX: '/',
   ABOUT: '/acerca-de',
-  TEACHERS_IDEAS: 'ideas-profesores',
+  TEACHERS_IDEAS: '/ideas-profesores',
   REGISTER: '/registrar'
 };
 
