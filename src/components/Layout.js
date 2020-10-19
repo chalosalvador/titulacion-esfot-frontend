6c4ed122f5aec27ba17a554d4f1097e673e81524
ChantalMorales <chantal.morales@epn.edu.ec>
2020-10-19 17:37:45 -0500

    arreglo del header para cada perfÃil

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,6 +5,7 @@ import React from 'react';
 import Navigation from './Navigation';
 import { Layout, Row, Col, Button, Popover } from 'antd';
 import { FacebookOutlined } from '@ant-design/icons';
+import Headers from './Headers';
 
 import logoEPN from '../images/Logotipo-ESFOT.png';
 import logoESFOT from '../images/Logotipo-ESFOT2.png';
@@ -28,44 +29,10 @@ const MainLayout = props => {
     <div className='app'>
       <Layout>
         <Row type='flex' justify='center' className='header-wrapper'>
-          <Col span={ 20 }>
+          <Col span={ 24 }>
 
             <Header className='header'>
-              <Row type='flex' justify='space-between' align='bottom'>
-                <Col xs={ 24 } md={ 6 } className='logo-wrapper'>
-                  <a href='https://www.epn.edu.ec/'>
-                    <img className='logo' src={ logoEPN } alt='' /></a>
-                </Col>
-
-                {/*<Col md={ 14 } align='right' className='main-menu'>*/}
-                {/*  <Navigation mode='horizontal' />*/}
-                {/*</Col>*/}
-
-                <Col xs={ 2 } align='right' className='responsive-menu-button'>
-                  <Popover content={ <Navigation mode='vertical' /> }
-                           trigger='click'
-                           placement='rightTop'
-                           overlayClassName='responsive-menu-wrapper'>
-                    <Button type='primary'>
-                      <svg viewBox='64 64 896 896'
-                           focusable='false'
-                           className=''
-                           data-icon='menu'
-                           width='1em'
-                           height='1em'
-                           fill='currentColor'
-                           aria-hidden='true'>
-                        <path d='M904 160H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0 624H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0-312H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8z'></path>
-                      </svg>
-                    </Button>
-                  </Popover>
-                </Col>
-
-                <Col xs={ 22 } md={ 4 } className='logos-social-header' align='right'>
-                  <a href='https://esfot.epn.edu.ec/'>
-                    <img className='logo' src={ logoESFOT } alt='' /></a>
-                </Col>
-              </Row>
+              <Headers />
             </Header>
           </Col>
         </Row>
